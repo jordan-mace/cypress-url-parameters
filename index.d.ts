db2c4e3d5508b30f51f322e1905aecee4adb9e1a
Jordan Mace <jordz98@gmail.com>
2024-05-28 20:22:03 +1000

    chore: File cleanup

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "parameters" {
-    export const addParameters: (originalFn: any, url: string, options: any) => void;
-    export const addParameter: (key: string, value: string, url: string) => string;
-    export const setParameter: (name: string, value: string) => Map<string, string>;
-    export const removeParameter: (name: string) => boolean;
-}
